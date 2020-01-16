9d3ad3110f1530648797f95c3f6c6849a727a35b
nicmlu <NicoleMLucien@gmail.com>
2020-01-15 22:17:22 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    belongs_to :networks
+
+    def actors_list
+        actors = []
+        self.actors.map do |a|
+            actors << a.name
+        end 
+        actors
+    end 
   
 end
