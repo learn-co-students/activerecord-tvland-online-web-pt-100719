7aa03e41210eb8256a61879380537a8564574c31
Adrian Bruno <adrian.j.bruno@gmail.com>
2020-01-19 20:25:01 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    array_of_actors = self.actors.collect do |actor|
+      "#{actor.first_name} #{actor.last_name}"
+    end
+    array_of_actors
+  end
+
 end
